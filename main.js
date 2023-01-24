e89ffa2f9211a1e5dcd0fea8c02dbc243f48e673
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-01-24 09:50:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const song = document.getElementsByTagName("audio");
 let btns = document.getElementsByTagName("button");
 btns = [...btns];
 
-const a = btns.map((btn, i) => {
+btns.map((btn, i) => {
     btn.addEventListener("click", () => song[i].play())
 })
